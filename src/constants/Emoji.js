7d87f901886946e0ec97c43958689d9db48684d4
PhamHaiLong99 <phlexile@gmail.com>
2021-10-17 15:52:28 +0700

    last commit homework

---
+++ src/constants/Emoji.js
@@ -0,0 +1,15 @@
+const EMOJI = [
+  {id: 'ICON_ANGRY', src: require('../../assets/emoji/angry.png')},
+  {id: 'ICON_CARE', src: require('../../assets/emoji/care.png')},
+  {id: 'ICON_HAHA', src: require('../../assets/emoji/haha.png')},
+  {id: 'ICON_LIKE', src: require('../../assets/emoji/like.png')},
+  {id: 'ICON_LOVE', src: require('../../assets/emoji/love.png')},
+  {id: 'ICON_SAD', src: require('../../assets/emoji/sad.png')},
+];
+// ICON_ANGRY: require('../../assets/emoji/angry.png'),
+// ICON_CARE: require('../../assets/emoji/care.png'),
+// ICON_HAHA: require('../../assets/emoji/haha.png'),
+// ICON_LIKE: require('../../assets/emoji/like.png'),
+// ICON_LOVE: require('../../assets/emoji/love.png'),
+// ICON_SAD: require('../../assets/emoji/sad.png'),
+export {EMOJI};
